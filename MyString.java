d6e3aff8d53057a2b7c77211f2df7f14378769ff
stefansor <ssorobay00@stuy.edu>
2018-11-25 14:57:10 -0500

    added all methods and errors need to change toString/constructor?

--- MyString.java
+++ MyString.java
@@ -1,35 +1,74 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
+
   public MyString(CharSequence s){
     data = new char[s.length()];
     for(int i = 0; i < data.length; i++){
       data[i] = s.charAt(i);
     }
   }
+
   public String toString(MyString s){
     String str = "";
+    if(this.data.length ==0){
+      return "";
+    }
     for(int j = 0; j < this.data.length; j++){
       str = str + this.data[j];
     }
     return str;
   }
+
   public char charAt(int a){
-    return (char)this.data[a];
+    try{
+      return this.data[a];
+    }
+    catch(IndexOutOfBoundsException e){
+      System.out.println("Index is out of bounds");
+      System.exit(1);
+    }
+    return this.data[a];
   }
+
   public int length(){
     return this.data.length;
   }
+
   public String subSequence(int start, int finish){
     String seq = "";
-    for(int k = start; k < finish; k++){
-      seq = seq + this.data.[k];
+    try{
+      for(int k = start; k < finish; k++){
+        seq = seq + this.data[k];
+      }
     }
+    catch(IndexOutOfBoundsException e){
+      System.out.println("Index out of bounds");
+      System.exit(1);
+    }
+    return seq;
   }
 
-
-
-
-
+  public int compareTo(CharSequence s){
+    try{
+      for(int i = 0; i < this.data.length; i++){
+        if(this.charAt(i) + 0 > s.charAt(i) + 0){
+          return 1;
+        }
+        if(this.charAt(i) + 0 < s.charAt(i) + 0){
+          return -1;
+        }
+      }
+    }
+    catch(NullPointerException e){
+      System.out.println("Value cannot be null");
+      System.exit(1);
+    }
+    catch(ClassCastException e){
+      System.out.println("Objects cannot be compared");
+      System.exit(1);
+    }
+    return 0;
+  }
 
 
 
